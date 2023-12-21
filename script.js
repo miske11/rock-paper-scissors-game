7447ef0acf1c3b3021fa1a967ef59b8069b2571b
miske <lukamisic2001@gmail.com>
2023-12-21 01:18:58 +0100

    Added function that returns result of the game

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
-function getComputerChoice() {
-    return Math.floor((Math.random() * 3)) + 1;
+function getComputerChoice() 
+{
+   return Math.floor((Math.random() * 3)) + 1;
 }
 
-console.log(getComputerChoice());
+function getRoundResult(playerSelection, computerSelection) 
+{
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+
+    if (playerSelection == "rock") {
+        if (computerSelection == "paper") {
+            return "You Lose! Paper beats Rock.";
+        }
+        else if (computerSelection == "scissors") {
+            return "You Win! Rock beats Scissors.";
+        }
+    }
+    if (playerSelection == "paper") {
+        if (computerSelection == "rock") {
+            return "You Win! Paper beats Rock.";
+        }
+        else if (computerSelection == "scissors") {
+            return "You Lose! Scissors beat Paper.";
+        }
+    }
+    if (playerSelection == "scissors") {
+        if (computerSelection == "paper") {
+            return "You Win! Scissors beat Paper.";
+        }
+        else if (computerSelection == "rock") {
+            return "You Lose! Rock beats Scissors.";
+        }
+    }
+}
+
+console.log(getRoundResult("rock","rock"));
